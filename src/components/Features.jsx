6b6d49607f0b36447be250f4c07464578e7a65e2
KingLyrics <ekom338.com@gmail.com>
2023-01-13 00:43:38 +0100

    Changed the color of the icons for the benefits screen

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -10,23 +10,23 @@ const Features = () => {
         <h1 className="text-[42px] text-Under-gray font-Ds text-center">
           Our Benefits
         </h1>
-        <div className="max-w-5xl mt-14 mx-auto grid grid-cols-3 gap-4">
+        <div className="max-w-5xl mt-14 mx-auto grid grid-cols-3 gap-x-28">
           <Card
-            icon={<BsFillBriefcaseFill size={70} className="text-Action" />}
+            icon={<BsFillBriefcaseFill size={70} className="text-Teal" />}
             title={"Many Useful components"}
             text={
               "Startup Framework contains components and complex blocks which can easily be integrated into almost any design."
             }
           />
           <Card
-            icon={<FaCompressAlt size={70} className="text-Action" />}
+            icon={<FaCompressAlt size={70} className="text-Teal" />}
             title={"Responsive Layout"}
             text={
               "We haven’t forgotten about responsive layout. With Startup Framework, you can create a website with full mobile support."
             }
           />
           <Card
-            icon={<FaDesktop size={70} className="text-Action" />}
+            icon={<FaDesktop size={70} className="text-Teal" />}
             title={"Retina Ready"}
             text={
               "Startup Framework works on devices supporting Retina Display. Feel the clarity in each pixel."
