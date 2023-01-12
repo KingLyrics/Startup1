4a062fe094d3ebb719a7c8904ed50c8e9c6681ac
KingLyrics <ekom338.com@gmail.com>
2023-01-12 23:54:05 +0100

    Content section added

---
+++ src/components/Content.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Content = () => {
+  return (
+    <div className='max-w-[1240px] mx-auto py-4 px-4 mt-56'>
+        <div className='max-w-5xl mx-auto text-center'>
+            <h1 className='uppercase text-Under-gray text-[14px]'>Design and Code</h1>
+            <p className='mt-2 max-w-3xl mx-auto text-[58px] text-Dark-blue'>Boost your workflow with Startup3</p>
+        </div>
+    </div>
+  )
+}
+
+export default Content
